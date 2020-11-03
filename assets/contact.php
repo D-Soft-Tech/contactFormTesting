9a89512f124a93efd9f34ff11c034e09e1eb9588
D-Soft-Tech <dynamicsofttechnology@gmail.com>
2020-11-03 13:59:19 +0100

    contactform

---
+++ assets/contact.php
@@ -0,0 +1,50 @@
+<?php
+
+// Email address verification
+function isEmail($email) {
+	return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+if(isset($_POST['submit'])) {
+
+    // Enter the email where you want to receive the message
+    $emailTo = 'oloyedeadebayoolawale@ymail.com';
+
+    $name = addslashes(trim($_POST['name']));
+    $clientEmail = addslashes(trim($_POST['email']));
+    $subject = addslashes(trim($_POST['subject']));
+    $message = addslashes(trim($_POST['message']));
+
+    $array = array('nameMessage' => '', 'emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
+
+    if($name == '') {
+    	$array['nameMessage'] = 'Empty name!';
+    }
+    if(!isEmail($clientEmail)) {
+        $array['emailMessage'] = 'Invalid email!';
+    }
+    if($subject == '') {
+        $array['subjectMessage'] = 'Empty subject!';
+    }
+    if($message == '') {
+        $array['messageMessage'] = 'Empty message!';
+    }
+    if($name != '' && isEmail($clientEmail) && $subject != '' && $message != '') {
+        // Send email
+        $message = "Message from: " . $name . "\r\n" . $message;
+		$headers = "From: " . $clientEmail . " <" . $clientEmail . ">" . "\r\n" . "Reply-To: " . $clientEmail;
+        
+        if(mail($emailTo, $subject . " (bootstrap contact form)", $message, $headers))
+        {
+            echo "Thank you for contacting us";
+        }
+        else{
+            echo "Error sending message";
+        }
+    }
+
+    echo json_encode($array);
+
+}
+
+?>
